83b6f57e869ef424c8f709b22a4d94e48de70148
KatyRouault <karo5633@colorado.edu>
2024-11-26 17:42:25 -0700

    commit

--- js/wa13portfolio.js
+++ js/wa13portfolio.js
@@ -1,8 +1,10 @@
+let proj;
 fetch('../portfolio.json/project.json')
     .then(response=>{
         return response.json();
     }).then(projects => {
         console.log(projects); //this variable should match the one about which is the projects//
+        proj =projects;
         parseData(projects);
     }).catch(err =>{
         console.log(`error ${err}`);
@@ -10,11 +12,36 @@ fetch('../portfolio.json/project.json')
 
 function parseData(data){
     for(let i=0; i<data.projects.length; i++){
-    document.getElementById("projects").innerHTML += `<a href="/projects_folder/${data.projects[i].subdomain}.html>
+    document.getElementById("projects").innerHTML += `<a href="../projects_folder/${data.projects[i].subdomain}.html">
     <div class="row project" id="${data.projects[i].subdomain}">
         <div class="projimg"><img src="../img/port (${i+1}).jpeg"></div>
         <div class="description"><h2>${data.projects[i].name}</h2><p class="subtitle">${data.projects[i].subtitle}</p>
         <p>${data.projects[i].abstract}</p></div></div></a>`;
+    }
+}
+
+for(b of document.querySelectorAll("#buttons button")){
+    b.addEventListener("click", e=>{
+        console.log(e.target.value);
+        sortProjects(e.target.value);
+    })
+}
+
+function sortProjects(button){
+    if(button == "clear"){
+        for(i=0;i<proj.projects.length; i++){
+            document.getElementById(proj.projects[i].subdomain).style.display = "flex";
+        }
+    }else if(button != undefined){
+            for(i=0; i<proj.projects.length;i++){
+                if(proj.projects[i].category.includes(button) == true){
+                    document.getElementById(proj.projects[i].subdomain).style.display = "flex";//shows the project if its in the selected category//
+                }else{
+                    document.getElementById(proj.projects[i].subdomain).style.display = "none";//hides the project if it is not a category//
+                }
+            }
+    }else{
+        console.log("error, button value is undefined");
     }
 }
 //TO DO//
--- js/wa13portfolio2.js
+++ js/wa13portfolio2.js
@@ -1,2 +1,31 @@
-let subdomain = window.location.href;
+let subdomain = window.location.href.slice(window.location.href.lastIndexOf("/")+1, window.location.href.lastIndexOf("."));
 console.log(subdomain);
+
+fetch('../portfolio.json/project.json')
+    .then(response=>{
+        return response.json();
+    }).then(projects => {
+        //console.log(projects); //this variable should match the one about which is the projects//
+        proj =projects;
+        findProjectInJSON(projects);
+        //parseData(projects);
+    }).catch(err =>{
+        console.log(`error ${err}`);
+    })
+
+function findProjectInJSON(projects){
+    for(let i=0; i<projects.projects.length; i++){
+        if(projects.projects[i].subdomain == subdomain){
+            buildPage(projects.projects[i]);
+            break;
+    }else{
+        continue;
+    }
+    }
+}
+
+function buildPage(project){
+    console.log(project);
+    document.getElementById("project").innerHTML += `<h1>${project.name}</h1>`
+}
+
