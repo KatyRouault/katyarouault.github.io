f2d8251effc477277af31e4d3d547bd394745caf
KatyRouault <karo5633@colorado.edu>
2024-12-01 12:59:59 -0700

    changed carousel

--- js/wa13portfolio.js
+++ js/wa13portfolio.js
@@ -1,6 +1,4 @@
 let proj;
-let slideIndex = 1;
-
 
 fetch('../portfolio.json/project.json')
     .then(response=>{
@@ -9,7 +7,6 @@ fetch('../portfolio.json/project.json')
         console.log(projects); //this variable should match the one about which is the projects//
         proj =projects;
         parseData(projects);
-        showSlides(slideIndex);
     }).catch(err =>{
         console.log(`error ${err}`);
     })
@@ -22,13 +19,6 @@ function parseData(data){
         <div class="description"><h2>${data.projects[i].name}</h2><p class="subtitle">${data.projects[i].subtitle}</p>
         <p>${data.projects[i].abstract}</p></div></div></a>`;
     } 
-    const carousel = document.getElementById("carousel");
-    for(let i=0; i<data.carousel.length; i++){
-        const div = document.createElement('div');
-        div.classList.add('carousel-item');
-        div.innerHTML = `<img class="img-carousel" src="${data.carousel[i].src}" alt="${data.carousel[i].alt}" style="width:30%">`;
-        carousel.appendChild(div);
-    }
 }
 
 for(b of document.querySelectorAll("#buttons button")){
@@ -56,40 +46,6 @@ function sortProjects(button){
     }
 }
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("carousel-item");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-      slides[slideIndex-1].style.display = "block";
-      //console.log(slides[i]);
-    }
-    console.log(slides[slideIndex-1]);
-    
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-
-    }
-    
-    dots[slideIndex-1].className += " active";
-  }
 
 
 //TO DO//
--- js/wa13portfolio2.js
+++ js/wa13portfolio2.js
@@ -1,18 +1,19 @@
 let subdomain = window.location.href.slice(window.location.href.lastIndexOf("/")+1, window.location.href.lastIndexOf("."));
-console.log(subdomain);
+let projImageIndex = 1;
 
+//Reading project json data
 fetch('../portfolio.json/project.json')
     .then(response=>{
         return response.json();
     }).then(projects => {
-        //console.log(projects); //this variable should match the one about which is the projects//
         proj =projects;
         findProjectInJSON(projects);
-        //parseData(projects);
+        showProjImage(projImageIndex);
     }).catch(err =>{
         console.log(`error ${err}`);
     })
 
+//Looping through projects and finding subdomain
 function findProjectInJSON(projects){
     for(let i=0; i<projects.projects.length; i++){
         if(projects.projects[i].subdomain == subdomain){
@@ -24,11 +25,42 @@ function findProjectInJSON(projects){
     }
 }
 
+//Building HTML from json
 function buildPage(project){
     document.getElementById("project").innerHTML += `<h1 class="title">${project.name}</h1>
     <div class="text"><h2>${project.subtitle}</h2>
     <p>${project.abstract}</p>
     <p>description: ${project.description}</p>
     <p>categrory: ${project.category}</p></div>`;
-    document.getElementById("project").innerHTML += `<img src="${project.mainimg}" alt="${project.name}" />`;
+
+    //Looping through project images and making carousel
+    const carousel = document.getElementById("carousel");
+    for(let i=0; i<project.projimages.length; i++){
+        const div = document.createElement('div');
+        div.classList.add('carousel-item');
+        div.innerHTML = `<img class="img-carousel" src="${project.projimages[i]}" style="width:30%">`;
+        carousel.appendChild(div);
+    }
+}
+
+// Next/previous controls
+function plusProjImage(n) {
+    showProjImage(projImageIndex += n);
+  }
+  
+//code to show project image selected and hide other images
+function showProjImage(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-item");
+    if (n > slides.length) {
+        projImageIndex = 1;
+    }
+    if (n < 1) {
+        projImageIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[projImageIndex-1].style.display = "block";
+    }
+    
 }
